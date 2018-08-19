6c2c984804cf16a5bd67903dd6f55618a275f1c9
Quentin <quentin@emmerge.com>
2018-08-19 10:27:30 -0400

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import LikeButton from './like_button.jsx';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import React from 'react';
+import LikeButton from './like_button.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 
 const domContainer = document.querySelector('#app');
