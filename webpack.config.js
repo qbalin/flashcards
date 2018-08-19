f0c608b4cc35d97c8b424161d506194d6904e242
Quentin <quentin@emmerge.com>
2018-08-19 08:58:10 -0400

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+};
