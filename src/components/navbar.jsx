8c2ba2a0b61dc1e9a6cba534a814cf06862d7da4
Quentin <quentin@emmerge.com>
2018-08-19 22:11:03 -0400

    Add icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
+import Octicon, { Zap } from '@githubprimer/octicons-react';
 
 export default () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-item nav-link active" href="#">
-          Home
-          <span className="sr-only">(current)</span>
-        </a>
-        <a className="nav-item nav-link" href="#">Features</a>
-        <a className="nav-item nav-link" href="#">Pricing</a>
-        <a className="nav-item nav-link disabled" href="#">Disabled</a>
-      </div>
-    </div>
+  <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="#">
+      <span style={{color: '#f5f500'}}>
+        <Octicon width={32} height={32} verticalAlign='middle' icon={Zap}/>
+      </span>
+      Flashcards
+    </a>
   </nav>
 );
