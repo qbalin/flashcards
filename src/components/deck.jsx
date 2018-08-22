97b5792b7f0cd2ba1e1dabaf2131a127ea8cc966
Quentin <quentin@emmerge.com>
2018-08-22 09:51:32 -0400

    Format cards. Add breadcrumbs

--- src/components/deck.jsx
+++ src/components/deck.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { loadFullDeck } from '../stores/decks';
+import { Link } from 'react-router-dom';
 
 class Deck extends React.Component {
   componentDidMount() {
@@ -8,20 +9,26 @@ class Deck extends React.Component {
   }
 
   renderSide = side => (
-    <p className="card-text" key={side.id}>{side.content}</p>
+    <li className="list-group-item" key={side.id}>{side.content}</li>
   )
 
   renderCard = card => (
     <div key={card.id} className="card">
-      <div className="card-body">
+      <ul className="list-group list-group-flush">
         {card.sides.map(this.renderSide)}
-      </div>
+      </ul>
     </div>
   )
 
   render() {
     return this.props.deck ? (
       <div>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><Link to='/'>Decks</Link></li>
+            <li class="breadcrumb-item"><a href="#">{this.props.deck.name}</a></li>
+          </ol>
+        </nav>
         <button type="button" className="btn btn-outline-primary">Start</button>
         <div className="card-deck">
           {this.props.deck.cards.map(this.renderCard)}
